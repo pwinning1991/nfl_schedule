6a077369dbdf4f9496caa5521344bed262049471
pwinning1991 <ftcpjw@yahoo.com>
2019-11-17 21:46:52 +0000

    pulled schedule info

--- nfl_schedule/get_schedule.py
+++ nfl_schedule/get_schedule.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 
 r = requests.get('http://nfl.com/schedules')
 r.raise_for_status
+#dev class='list-matchup-row-center'
 
 soup = BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify())
+
+for tag in soup.find_all("div", class_="list-matchup-row-team"):
+  print(tag,"\n")
